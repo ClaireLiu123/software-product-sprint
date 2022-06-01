b2111c3a7da3f8735a7c1038b42fae03171a4677
Claire Liu <claireliu123@berkeley.edu>
2022-06-01 05:17:55 +0000

    added image and edit script

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I like boba', 'I like ice cream', 'I am a gamer', 'I am a professional figure skater'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
